dedafeedb5a83567aff618d8a8a8e0729b0c924d
H-M-Nizum <hmnizum1714032@gmail.com>
2024-04-23 14:25:20 +0600

    3. Write a Python program to remove an element from a list.

---
+++ 3_Remove_Element.py
@@ -0,0 +1,32 @@
+def remove_element(l):
+    while True:
+    
+        print('\nIf you want remove element base on index, press : 1 ')
+        print('If you want remove element base on element, press : 2 ')
+        print('If you want stop this process, press : 3\n')
+        chose = int(input('Enter your Number : '))
+        if chose == 1:
+            index = int(input('Enter a index number : '))
+            if index < len(l):
+                l.pop(index)
+                print(f"Successfully {index}th element is removed.")
+            else:
+                print('This index is out of range')
+        elif chose == 2:
+            element = int(input('Enter a element : '))
+            if element in l:
+                l.remove(element)
+                print(f"Successfully removed : {element}.")
+            else:
+                print('This element is not found')
+        elif chose == 3:
+            print("After remove element this list is : ", l)
+            break
+        else:
+            print('Please Enter a correct number!')
+
+
+
+if __name__ == "__main__":
+    l = list(map(int, input('Enter your list : ').split()))
+    remove_element(l)
